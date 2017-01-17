5caace8feb461b49c2604ac91bea3757677aa075
all360 <adrianjramirez@gmail.com>
2017-01-17 10:45:41 -0800

    add gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ImageDayComponent } from './image-day/image-day.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    ImageDayComponent
+    ImageDayComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
